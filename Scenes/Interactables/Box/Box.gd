2d46144548174e02a5b181e3f5d88e070bd9e563
Ed Jones <edjones1999@gmail.com>
2020-07-12 15:02:44 +1000

    Improved grid movement

--- Scenes/Interactables/Box/Box.gd
+++ Scenes/Interactables/Box/Box.gd
@@ -1,2 +1,11 @@
 extends RigidBody2D
+var target_pos: Vector2
 
+func _physics_process(delta):
+	if abs(position.distance_to(target_pos)) <= 3:
+		apply_central_impulse(-linear_velocity)
+
+
+func _on_Box_body_entered(body):
+	if body.is_in_group("Players"):
+		target_pos = position + linear_velocity.normalized()*64
--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,21 +1,38 @@
-extends KinematicBody2D
+extends Area2D
 
-var velocity: float = 180
+const velocity: float = 4.0
 var direction: Vector2
 var target_pos: Vector2
 var push = 80
+const tile_size = 64
+onready var ray = $RayCast2D
+onready var tween = $Tween
 
-func _physics_process(delta):
-	if position.distance_to(target_pos) > 1:
-        var _collision = move_and_collide(direction*velocity*delta)
+#func _physics_process(delta):
+#	if position.distance_to(target_pos) > 1:
+#        var _collision = move_and_collide(direction*velocity*delta)
+#
+#	for index in get_slide_count():
+#		var collision = get_slide_collision(index)
+#		if collision.collider.is_in_group("Doors"):
+#			collision.collider.nextRoom()
+
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+
+func move(direction):
+	if tween.is_active():
+        return
+	ray.cast_to = direction * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		move_tween(direction)
+		
+func move_tween(direction):
+	tween.interpolate_property(self, "position",
+		position, position + direction * tile_size,
+		1.0/velocity, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
 	
-	for index in get_slide_count():
-		var collision = get_slide_collision(index)
-		if collision.collider.is_in_group("bodies"):
-			collision.collider.apply_central_impulse(-collision.normal * push)
-		if collision.collider.is_in_group("Doors"):
-			collision.collider.nextRoom()
-
-func move(_direction):
-	direction = _direction
-	target_pos = position + direction*64
